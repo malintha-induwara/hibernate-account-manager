e050ad4cfd9924ac1dbcfd58955f0b6149288f15
Induwara <induwara2k@gmail.com>
2024-02-14 18:50:00 +0530

    hibernate config added

---
+++ src/main/java/lk/ijse/accountManager/config/SessionFactoryConfig.java
@@ -0,0 +1,36 @@
+package lk.ijse.accountManager.config;
+
+import lk.ijse.accountManager.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class SessionFactoryConfig {
+
+    public static SessionFactoryConfig sessionFactoryConfig;
+    private final SessionFactory sessionFactory;
+
+
+    private SessionFactoryConfig(){
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().loadProperties("hibernate.properties").build();
+        Metadata metadata = new MetadataSources(serviceRegistry)
+                .getMetadataBuilder()
+                .build();
+       this.sessionFactory= metadata.buildSessionFactory();
+
+    }
+
+    public static SessionFactoryConfig getInstance(){
+        return (sessionFactoryConfig==null)?sessionFactoryConfig=new SessionFactoryConfig():sessionFactoryConfig;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+
+
+}
+
